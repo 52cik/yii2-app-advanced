b2d0cbf8539a8d9053fd0ce29d73558c4d0bd798
楼教主 <cik520@qq.com>
2016-03-16 12:34:09 +0800

    添加 初始化控制器操作

--- environments/dev/console/controllers/InitController.php
+++ environments/dev/console/controllers/InitController.php
@@ -3,15 +3,36 @@ namespace console\controllers;
 
 use Yii;
 use yii\console\Controller;
+use common\models\User;
 
 /**
- * Init controller
+ * 初始化控制器
  */
 class InitController extends Controller
 {
 
+    /**
+     * 默认动作
+     */
     public function actionIndex()
     {
         echo "一些初始化操作...\n";
     }
+
+    /**
+     * 添加超级管理员
+     */
+    public function actionAdmin()
+    {
+        $model = new User();
+
+        $model->username = 'admin';
+        $model->password = '123123';
+        $model->email = 'admin@admin.com';
+        $model->generateAuthKey();
+
+        echo $model->save() ? 'success!' : 'failed!';
+        echo PHP_EOL;
+    }
+
 }
