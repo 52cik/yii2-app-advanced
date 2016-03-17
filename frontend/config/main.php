a817323b53cd6928203b2ded3569152b1d70d797
楼教主 <cik520@qq.com>
2016-03-17 10:56:03 +0800

    修改 urlManage 默认配置

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -13,7 +13,7 @@ return [
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'user' => [
-            'identityClass' => 'common\models\User',
+            'identityClass' => 'common\models\User', // 前后端用户分离时修改
             'enableAutoLogin' => true,
         ],
         'log' => [
@@ -31,8 +31,17 @@ return [
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
             'showScriptName' => false,
-            'rules' => [
+            'suffix' => '.html',
+            'rules' => [ // 规则按顺序匹配，所以子匹配要写前面
+                '<controller:\w+>-<action:\w+>-<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>-<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+>' => '<controller>/index',
+
+                // 可以指定请求模式
+                // 'PUT <controller:[\w-]+>/<id:\d+>'    => '<controller>/update',
+                // 'DELETE <controller:[\w-]+>/<id:\d+>' => '<controller>/delete',
             ],
         ],
         */
